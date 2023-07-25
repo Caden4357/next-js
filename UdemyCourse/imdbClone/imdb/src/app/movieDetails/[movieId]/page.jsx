1722b5e45f3e1b90a99411db1045c88e8b2576fe
Caden Wilcox <wilcox4357@gmail.com>
2023-07-24 18:55:32 -0700

    working on one movie component

--- UdemyCourse/imdbClone/imdb/src/app/movieDetails/[movieId]/page.jsx
+++ UdemyCourse/imdbClone/imdb/src/app/movieDetails/[movieId]/page.jsx
@@ -1,12 +1,19 @@
+const API_KEY = process.env.API_KEY;
+
 import React from 'react';
 
-const MovieDetails = ({params: {movieId}}) => {
-    console.log(movieId);
+
+const MovieDetails = async ({params: {movieId}}) => {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US` , {next:{revalidate:10000}});
+    const movie = await res.json();
+    console.log(movie);
     return (
-        <div>
-            Movie Details
-            <br />
-            {movieId}
+        <div className='w-8/12 border border-red-200 mx-auto'>
+            <h2 className='text-3xl'>{movie.title}</h2>
+            <p>Release Date: {movie.release_date}</p>
+            <p>Runtime: {movie.runtime} Min</p>
+            <img className="w-3/6" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} alt={movie.title || movie.name} />
+            <p>{movie.overview}</p>
         </div>
 )}
 
